03b23462411d37a99f98b35eb5cdc79374095baa
wertrain <wertrain@gmail.com>
2020-03-09 23:53:58 +0900

    ファイルからの読み込みに対応

--- text-proofreader.js
+++ text-proofreader.js
@@ -1,17 +1,30 @@
 // @see https://www.npmjs.com/package/commander
 const program = require('commander');
 program
-  .option('-t, --text <content>', 'proofreading text');
+  .option('-t, --text <content>', 'proofreading text')
+  .option('-f, --file <filepath>', 'proofreading text from file');
 program.parse(process.argv);
 
+const textlint = require("textlint");
+const textLineEngine = new textlint.TextLintEngine({
+  configFile: '.textlintrc'
+});
+
 if (program.text) {
-  const textlint = require("textlint");
-  const textLineEngine = new textlint.TextLintEngine({
-    configFile: '.textlintrc'
-  });
   textLineEngine.executeOnText(program.text, '.txt').then(results => {
     process.stdout.write(JSON.stringify(results[0].messages));
   }, error => {
     process.stdout.write("textlint error occurred." + error);
+  });
+}
+
+if (program.file) {
+  const fs = require("fs-extra");
+  fs.readFile(program.file, "utf-8", (err, data) => {
+    textLineEngine.executeOnText(data, '.txt').then(results => {
+      process.stdout.write(JSON.stringify(results[0].messages));
+    }, error => {
+      process.stdout.write("textlint error occurred." + error);
+    });
   });
 }
