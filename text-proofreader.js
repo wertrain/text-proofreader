ba48d56491dc18454f994001b52a7d6b9412dc6d
wertrain <wertrain@gmail.com>
2020-03-11 00:16:08 +0900

    リファクタ

--- text-proofreader.js
+++ text-proofreader.js
@@ -5,24 +5,23 @@ program
   .option('-f, --file <filepath>', 'proofreading text of file');
 program.parse(process.argv);
 
-const textlint = require("textlint");
-const textLineEngine = new textlint.TextLintEngine({
-  configFile: '.textlintrc'
-});
-
-if (program.text) {
-  textLineEngine.executeOnText(program.text, '.txt').then(results => {
+let proofreadingText = function(text) {
+  const textlint = require("textlint");
+  const textLineEngine = new textlint.TextLintEngine({
+    configFile: '.textlintrc'
+  });
+  textLineEngine.executeOnText(text, '.txt').then(results => {
     process.stdout.write(JSON.stringify(results[0].messages));
   }, error => {
     process.stdout.write("textlint error occurred." + error);
   });
+}
+
+if (program.text) {
+  proofreadingText(program.text);
 } else if (program.file) {
   const fs = require("fs-extra");
   fs.readFile(program.file, "utf-8", (err, data) => {
-    textLineEngine.executeOnText(data, '.txt').then(results => {
-      process.stdout.write(JSON.stringify(results[0].messages));
-    }, error => {
-      process.stdout.write("textlint error occurred." + error);
-    });
+    proofreadingText(data);
   });
 }
